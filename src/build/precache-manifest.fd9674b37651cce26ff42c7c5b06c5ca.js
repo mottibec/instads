69a8496a82342fa57e130278b5db1b4f0df374a6
Motti Bechhofer <MBechhofer@chorus.cloud>
2019-12-19 14:01:55 +0200

    add react app

---
+++ src/build/precache-manifest.fd9674b37651cce26ff42c7c5b06c5ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec76273c7aaee320ac10c7e1e42aa412",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a6b08901ccbb448b75f",
+    "url": "/static/css/main.07aca493.chunk.css"
+  },
+  {
+    "revision": "6eb3a95a96ab63f8eca4",
+    "url": "/static/js/2.89bd7421.chunk.js"
+  },
+  {
+    "revision": "84ca184623799c8fc71c1a5c64ce127d",
+    "url": "/static/js/2.89bd7421.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4a6b08901ccbb448b75f",
+    "url": "/static/js/main.f0e0bd9c.chunk.js"
+  },
+  {
+    "revision": "e1044d0374f934a66522",
+    "url": "/static/js/runtime-main.b03dff36.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);

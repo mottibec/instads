0710aa6829ef25a9449b1fcddab6c70c73b10674
Motti Bechhofer <MBechhofer@chorus.cloud>
2019-12-21 23:19:45 +0200

    remove google login

---
+++ src/build/precache-manifest.71c659eb845ac6c3700637f41a4a4236.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2041a331ee8fe1a82687318e72f2d1e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a107b05924f705965666",
+    "url": "/static/css/main.e30ae39c.chunk.css"
+  },
+  {
+    "revision": "dc327937d1152b44d9ca",
+    "url": "/static/js/2.90a76019.chunk.js"
+  },
+  {
+    "revision": "84ca184623799c8fc71c1a5c64ce127d",
+    "url": "/static/js/2.90a76019.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a107b05924f705965666",
+    "url": "/static/js/main.b43219a5.chunk.js"
+  },
+  {
+    "revision": "e1044d0374f934a66522",
+    "url": "/static/js/runtime-main.b03dff36.js"
+  },
+  {
+    "revision": "f5c07640d6e7221204d83996446d3464",
+    "url": "/static/media/bg.f5c07640.svg"
+  },
+  {
+    "revision": "252f12f9fa8c98e74919672476fd29b6",
+    "url": "/static/media/logo.252f12f9.svg"
+  }
+]);
